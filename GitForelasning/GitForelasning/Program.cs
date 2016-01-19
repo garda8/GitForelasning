db319f161a6bc63201541a1304f6757afb03bc91
Davaeorn <davaeorn@gmail.com>
2016-01-19 09:58:46 +0100

    Davaeorn commit fix

--- GitForelasning/GitForelasning/Program.cs
+++ GitForelasning/GitForelasning/Program.cs
@@ -12,6 +12,7 @@ namespace GitForelasning
         {
             Console.WriteLine("Start");
             Console.WriteLine("scKeijser");
+            Console.WriteLine("Davaeorn");
             Console.WriteLine("proxy51");
             Console.WriteLine("AntonHoog");
             Console.ReadKey();
